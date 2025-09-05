241f4db203e1a605c20706ed2d3e80a5da7228f1
IsaiasMaxter <168202561+IsaiasMaxter@users.noreply.github.com>
2025-09-05 09:20:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('loginForm');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const user = document.getElementById('username').value;
+  const pass = document.getElementById('password').value;
+
+  if (user === "isaias" && pass === "01011990") {
+    message.style.color = "green";
+    message.textContent = "Login realizado com sucesso!";
+  } else {
+    message.style.color = "red";
+    message.textContent = "Usuário ou senha incorretos.";
+  }
+});
